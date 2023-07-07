48b3e1097760be5aea533a8a44622ded7389de35
MohamedAzarudeen <azartechnet@gmail.com>
2023-07-07 18:32:36 +0530

    07-07-2023

--- ExampleP1/Program.cs
+++ ExampleP1/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("Welcome to the Application");
+﻿int pid = 1001;
+String pname = "apple";
+float f1 = 2.5f;
+/*Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Welcome to the Application");*/
+Console.WriteLine("Your ProductName::"+pname);
+Console.WriteLine("Your fractional::"+f1);
+Console.WriteLine("Your ProductId::"+pid);
---
+++ OperatorsP1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int a = 100, b = 200;
+Console.WriteLine("Arithmetic Operators");
+Console.WriteLine(a + b);
+Console.WriteLine(a*b);
+Console.WriteLine(a/b);
+Console.WriteLine(a%b);
+Console.WriteLine("Relational Operators");
+Console.WriteLine("Lessthan::"+(a<b));
+Console.WriteLine("Lessthanequal to::"+(a<=b));
+Console.WriteLine("Equal to::" + (a == b));
+Console.WriteLine("NotEqual to::" + (a != b));
