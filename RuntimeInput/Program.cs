7ab818b0cfd588c0e87d763216bdd198ea03123c
MohamedAzarudeen <azartechnet@gmail.com>
2023-07-10 18:17:54 +0530

    10-07-2023

---
+++ RuntimeInput/Program.cs
@@ -0,0 +1,8 @@
+﻿/*int pid;
+Console.WriteLine("Enter the Pid is::");
+pid = Convert.ToInt32(Console.ReadLine());
+String pname;
+Console.WriteLine("Enter the pname is::");
+pname = Console.ReadLine();
+Console.WriteLine("Your Pid is::" + pid + "Your Pname is::" + pname);*/
+
