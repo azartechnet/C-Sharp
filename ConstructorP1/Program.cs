1e60c793da55e89a9d07483e1aca44a4d2fc01b9
MohamedAzarudeen <azartechnet@gmail.com>
2023-07-18 12:21:16 +0530

    18-07-2023

---
+++ ConstructorP1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+class Sample
+{
+    Sample() 
+    {
+        Console.WriteLine("default cons...");
+    }
+    Sample(int x,String name) 
+    {
+        Console.WriteLine("Your value is::" + x + "" + name);
+    }
+    public static void Main(String[] args)
+    {
+         new Sample();
+        new Sample(1001, "Azar");
+    }
+}
---
+++ oopsp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Employee
+{
+    public int empid;
+    public String empname;
+    public int age;
+
+    void get1()
+    {
+        Console.WriteLine("Enter the empid is::");
+        empid=Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the empname is::");
+        empname = Console.ReadLine();
+        Console.WriteLine("Enter the age is::");
+        age=Int32.Parse(Console.ReadLine());
+    }
+    void display()
+    {
+        Console.WriteLine("Your value is::" + empid + "" + empname + ""+age);
+    }
+    public static void Main(String[] args)
+    {
+        Employee employee = new Employee();
+        employee.get1();
+        employee.display();
+    }
+}
