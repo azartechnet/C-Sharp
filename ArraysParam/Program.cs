deebdac740548c3db975504658b8dd8d093a0369
MohamedAzarudeen <azartechnet@gmail.com>
2023-07-27 12:45:31 +0530

    27-07-23

---
+++ ArraysParam/Program.cs
@@ -0,0 +1,15 @@
+﻿class Sample
+{
+    public void show(params object[] val)
+    {
+        for(int i=0;i<val.Length; i++)
+        {
+            Console.WriteLine(val[i]);
+        }
+    }
+    static void Main(string[] args)
+    {
+        Sample sample = new Sample();
+        sample.show(1,2,3,4,5,'a',"Azar");
+    }
+}
---
+++ InheritanceP1/Program.cs
@@ -0,0 +1,28 @@
+﻿class B
+{
+     public int pid;
+    public void get1()
+    {
+       pid=Int32.Parse(Console.ReadLine());
+    }
+}
+class D1 : B
+{
+    public String pname = "apple";
+}
+class D2:D1
+{
+    public void get2()
+    {
+        Console.WriteLine("Base class value is::" + pid+""+pname);
+    }
+}
+class MainDemo
+{
+    static void Main(string[] args)
+    {
+        D2 s1=new D2();
+        s1.get1();
+        s1.get2();
+    }
+}
