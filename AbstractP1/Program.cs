99668ff4bfd45239445ed224f06cefa51eb21e0a
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-02 11:15:00 +0530

    02-08-2023

---
+++ AbstractP1/Program.cs
@@ -0,0 +1,27 @@
+﻿public abstract class Sample
+{
+    public abstract void get1();
+    public void get2()
+    {
+        Console.WriteLine("Normal Methods..");
+    }
+
+}
+public class Foo:Sample
+{
+
+    override
+    public void get1()
+    {
+        Console.WriteLine("This is Abstract Class");
+    }
+}
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Foo f1=new Foo();
+        f1.get1();
+        f1.get2();
+    }
+}
