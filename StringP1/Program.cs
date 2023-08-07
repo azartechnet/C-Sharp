84017820d9c9a682fab0202daa8bb359fce9c67a
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-07 16:44:03 +0530

    07-08-2023

--- StringP1/Program.cs
+++ StringP1/Program.cs
@@ -1,8 +1,25 @@
-﻿string s1 = "Welcome-1";
-String s2 = "Welcome-2";
+﻿/*string s1 = "Welcome-1";
+String s2 = "Welcome-2";*/
 
-Console.WriteLine(s1+""+s2);
+
+string s1 = " Hello c#,How are you? ";
+
+char[] ch = new char[15];
+
+/*Console.WriteLine(s1+""+s2);
 
 Console.WriteLine(s1.CompareTo(s2));
 
-Console.WriteLine(s2.Equals(s2));
+Console.WriteLine(s2.Equals(s2));*/
+
+//s1.CopyTo(10, ch, 0, 12);
+
+//Console.WriteLine(ch);
+
+//string s2=s1.Trim(); 
+
+//string s3 = s1.TrimStart();
+
+string s3 = s1.TrimEnd();
+
+Console.WriteLine(s3);
