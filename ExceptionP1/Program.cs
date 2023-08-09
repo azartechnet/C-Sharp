89c8e69b7c52d2ea32ddd4f41a31cadbaeb9bd38
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-09 13:22:05 +0530

    09-08-2023

---
+++ ExceptionP1/Program.cs
@@ -0,0 +1,45 @@
+﻿class Sample
+{
+    void get1()
+    {
+        try
+        {
+            int a = 100;
+            int c;
+            c = a / 0;
+            Console.WriteLine(c);
+        }
+        catch (ArithmeticException e) 
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        get2();
+    }
+    void get2()
+    {
+        try
+        {
+            string s =null;
+            Console.WriteLine(s.Length);
+        }
+        catch(NullReferenceException e)
+        {
+            Console.Write(e.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("finally block");
+        }
+        get3();
+    }
+    void get3()
+    {
+        Console.WriteLine("This is normal function...");
+    }
+    static void Main(string[] args)
+    {
+        Sample s1= new Sample();    
+        s1.get1();
+    }
+}
---
+++ ExceptionP2/Program.cs
@@ -0,0 +1,34 @@
+﻿public class AzarException:Exception
+{
+    public AzarException(String msg):base(msg)
+    {
+
+    }
+}
+public class TestApplication
+{
+
+    static void validate(int age)
+    {
+        if (age < 18)
+        {
+            throw new AzarException("Sorry");
+        }
+        else
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        try
+        {
+            validate(20);
+        }
+        catch (AzarException e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+}
+}
